d49aa66cefd70e6e091f3fea61daef2938e05e7d
Tiago Alexandre Leme Barbosa <81383860+TiagoLeme@users.noreply.github.com>
2025-06-09 23:01:15 -0300

    Update App.js
    Parada Obrigatória 2

--- src/App.js
+++ src/App.js
@@ -7,19 +7,26 @@ import './style.css';
 function App() {
   return (
     <div className="container">
-      <header>
-        <h1>Loja de Livros - Ainda em construção</h1>
+      <header role="banner">
+        <h1 tabIndex="0">Loja de Livros - Ainda em construção</h1>
       </header>
-      <main>
-        <ProductList />
-        <Cart />
+      <main role="main">
+        <section aria-labelledby="product-list">
+          <h2 id="product-list" className="visually-hidden">Lista de Produtos</h2>
+          <ProductList />
+        </section>
+        <section aria-labelledby="shopping-cart">
+          <h2 id="shopping-cart" className="visually-hidden">Carrinho de Compras</h2>
+          <Cart />
+        </section>
       </main>
-      <footer>
-        <p>© 2025 Loja de Livros - Meu primeiro aplicativo fazendo Fullstack </p>
+      <footer role="contentinfo">
+        <p>© 2025 Loja de Livros - Meu primeiro aplicativo fazendo Fullstack</p>
       </footer>
     </div>
   );
 }
 
 export default App;
+
 
